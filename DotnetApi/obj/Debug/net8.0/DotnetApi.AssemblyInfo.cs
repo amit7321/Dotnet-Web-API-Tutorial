64d3990d19602eba3f274d0c3668eda14899f79b
amit7321 <amit.hasan7321@gmail.com>
2024-08-26 02:12:00 +0600

    change in the project

--- DotnetApi/obj/Debug/net8.0/DotnetApi.AssemblyInfo.cs
+++ DotnetApi/obj/Debug/net8.0/DotnetApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+366f83a903ed0fc6208107717696e2a5476e090d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5e87e864c9acab494f7b03228d456e7436e42a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
